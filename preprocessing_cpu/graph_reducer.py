194b8823bc9ddd17b99f4c1da18425140b9dc745
dleloutre <daniela.leloutre@gmail.com>
2024-03-05 19:44:38 -0300

    agrego reduccion por subgrafo y pequeño fix en mcgs

--- preprocessing_cpu/graph_reducer.py
+++ preprocessing_cpu/graph_reducer.py
@@ -31,24 +31,15 @@ class GraphReducer():
 class IterativeGraphReducer(GraphReducer):
   def reduce(self, G):
     G = G.copy()
-    print("G.nodes", G.nodes(), "G.edges", G.edges())
-    # Set de nodos ya reducidos
     reduced_nodes = set()
-    # Ordenar los nodos en orden descendente por grado
     nodes = sorted(G, key=G.degree, reverse=True)
-    print("nodes", nodes)
     for node in nodes:
-      print("reduced_nodes", reduced_nodes)
       if node not in reduced_nodes:
-        print("node", node)
-        # Obtener los vecinos del nodo
         neighbors = list(G.predecessors(node)) + list(G.successors(node))
-        print("neighbors", neighbors)
         for neighbor in neighbors:
           if neighbor not in reduced_nodes:
             # Apropiarse de las aristas de los vecinos
             neighbors_of_neighbor = list(G.predecessors(neighbor)) + list(G.successors(neighbor))
-            print("neighbors_of_neighbor", neighbors_of_neighbor)
             for n in neighbors_of_neighbor:
               if n != node:
                 # Agregar la arista en la dirección correcta
@@ -56,11 +47,8 @@ class IterativeGraphReducer(GraphReducer):
                   G.add_edge(node, n)
                 else:
                   G.add_edge(n, node)
-            # Marcar el vecino como reducido
             reduced_nodes.add(neighbor)
-            # Eliminar el vecino
             G.remove_node(neighbor)
-        # Marcar el nodo como reducido
         reduced_nodes.add(node)
     return G
 
@@ -79,8 +67,7 @@ class TransitiveGraphReducer(GraphReducer):
     for node in nodes:
       if G.degree(node) == 1:
         nodes_to_remove.add(node)
-    for node in nodes_to_remove:
-      G.remove_node(node)
+    G.remove_nodes_from(nodes_to_remove)
     return G
   
   def transitivity(self, G):
@@ -94,67 +81,6 @@ class TransitiveGraphReducer(GraphReducer):
               if G.degree(neighbor) == 0:
                 G.remove_node(neighbor)
     return G
-
-
-class RecursiveGraphReducer(GraphReducer):
-  def out_reduce(self, G):
-    sorted_out_degrees = sorted(G.out_degree, key=lambda x: x[1], reverse=True)
-    if len(G.nodes()) < 180 or sorted_out_degrees[0][1] <= 2:
-        return G
-
-    sd = sorted_out_degrees[0]
-    successors = list(G.out_edges(sd[0]))
-    supernode = successors[0][1]
-    edges_to_add = set()
-    nodes_to_remove = set()
-    reduced = set()
-
-    for s in successors:
-      if s[1] in reduced:
-        continue
-      in_edges = [(e[0], supernode) for e in G.in_edges(s[1])]
-      out_edges = [(supernode, e[1]) for e in G.out_edges(s[1])]
-      edges_to_add.update(in_edges)
-      edges_to_add.update(out_edges)
-      if s[1] != supernode:
-        nodes_to_remove.add(s[1])
-      reduced.add(s[1])
-
-    G.add_edges_from(edges_to_add)
-    G.remove_nodes_from(nodes_to_remove)
-    return self.out_reduce(G)
-
-  def in_reduce(self, G):
-      sorted_in_degrees = sorted(G.in_degree, key=lambda x: x[1], reverse=True)
-      if len(G.nodes()) < 180 or sorted_in_degrees[0][1] <= 2:
-          return G
-
-      sd = sorted_in_degrees[0]
-      predecessors = list(G.in_edges(sd[0]))
-      supernode = predecessors[0][1]
-      edges_to_add = set()
-      nodes_to_remove = set()
-      reduced = set()
-
-      for p in predecessors:
-        if p[0] in reduced:
-          continue
-        in_edges = [(e[0], supernode) for e in G.in_edges(p[0])]
-        out_edges = [(supernode, e[1]) for e in G.out_edges(p[0])]
-        edges_to_add.update(in_edges)
-        edges_to_add.update(out_edges)
-        if p[0] != supernode:
-            nodes_to_remove.add(p[0])
-        reduced.add(p[0])
-
-      G.add_edges_from(edges_to_add)
-      G.remove_nodes_from(nodes_to_remove)
-      return self.in_reduce(G)
-
-  def reduce(self, G):
-      G = self.out_reduce(G)
-      G = self.in_reduce(G)
-      return G
   
 class MCGSReducer():
   def __init__(self):
@@ -162,3 +88,4 @@ class MCGSReducer():
   
   def reduce(self, G):
     return self.mcgs.run_sampling(G, rate=0.5)
+
