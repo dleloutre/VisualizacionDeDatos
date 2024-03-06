8ef0b53f7232cb1a8ee02b4df3e560a0558a6a63
dleloutre <daniela.leloutre@gmail.com>
2024-03-05 21:51:41 -0300

    refactor

--- preprocessing_cpu/main.py
+++ preprocessing_cpu/main.py
@@ -1,30 +1,43 @@
 import sys
+import time
 import pandas as pd
 from graph_reducer import *
 
-# chequear
 def directed_diameter(G):
     shortest_path_lengths = dict(nx.all_pairs_shortest_path_length(G))
     diameter = 0
     
-    for source, lengths in shortest_path_lengths.items():
+    for _source, lengths in shortest_path_lengths.items():
         max_length = max(lengths.values())
         diameter = max(diameter, max_length)
     
     return diameter
 
-def main():
-    reduceFile = sys.argv[1]
-    df = pd.read_csv(reduceFile, sep=';', header=0, names=["v1", "v2", "_ignore"])
-    G = nx.from_pandas_edgelist(df, source='v1', target='v2', create_using=nx.DiGraph())
-    print("Cantidad inicial de nodos: ", len(G.nodes()))
-    print("Cantidad inicial de aristas: ", len(G.edges()))
+def show_properties(G):
+    print("Cantidad de nodos: ", len(G.nodes()))
+    print("Cantidad de aristas: ", len(G.edges()))
     print("Coeficiente de clustering: ", nx.average_clustering(G))
     print("Diametro: ", directed_diameter(G))
     print("Grado promedio: ", sum(d[1] for d in nx.degree(G))/nx.number_of_nodes(G))
     print("Densidad: ", nx.density(G))
-    # IterativeGraphReducer | MCGSReducer | TransitiveGraphReducer
-    reducer =  TransitiveGraphReducer()
+
+def get_reducer(reducerType):
+    if reducerType == 'transitive':
+        return TransitiveGraphReducer()
+    if reducerType == 'mcgs':
+        return MCGSReducer()
+    if reducerType == 'degree':
+        return ByDegreeGraphReducer()
+    return None
+
+def get_graph(fileName):
+    df = pd.read_csv(fileName, sep=';', header=0, names=["v1", "v2", "_ignore"])
+    return nx.from_pandas_edgelist(df, source='v1', target='v2', create_using=nx.DiGraph())
+
+def main():
+    G = get_graph(sys.argv[1])
+    reducer = get_reducer(sys.argv[2])
+    show_properties(G)
     # reduce connected components separately and then join again
     subgraphs = []
     connected_components = nx.weakly_connected_components(G)
@@ -32,13 +45,10 @@ def main():
         subgraph = G.subgraph(connected_component).copy()
         subgraphs.append(reducer.reduce(subgraph))
     Gs = nx.compose_all(subgraphs)
-    print("Cantidad final de nodos: ", len(Gs.nodes()))
-    print("Cantidad final de aristas: ", len(Gs.edges()))
-    print("Coeficiente de clustering: ", nx.average_clustering(Gs))
-    print("Diametro: ", directed_diameter(Gs))
-    print("Grado promedio: ", sum(d[1] for d in nx.degree(Gs))/nx.number_of_nodes(Gs))
-    print("Densidad: ", nx.density(Gs))
+    show_properties(Gs)
     #nx.write_edgelist(Gs, '/Users/danileloutre/Downloads/reduced_lasalle.csv', delimiter=';', data=True, encoding='utf-8')
 
 if __name__=="__main__":
+    start_time = time.time()
     main()
+    print("--- %s seconds ---" % (time.time() - start_time))
