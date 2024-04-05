2e3a9e1c3088673215170ddf3a56754cc11a9a6f
chiabauni <chiabauni@gmail.com>
2024-04-04 21:09:47 -0300

    Desacoplo funciones de crossing_edges

--- visualization/test.js
+++ visualization/test.js
@@ -6,17 +6,44 @@ import partiesJson from "./data/parties.json";
 
 let nodePositions = [];
 let nodesIds = [];
+let crossing_edges = [];
 let instancedEdges;
 let instancedNodes;
 let scene, camera, renderer, stats;
-
+const crossingEdgeFile = "/data/edges/df_crossed_edges_reduced.csv";
+const nodeFilePrefix = "/data/nodes/mcgs_reduced_";
+const edgeFilePrefix = "/data/edges/mcgs_reduced_";
+const edgeFileSuffix = ".csv";
+const nodeFileSuffix = "_FR.csv";
+const fileKeys = [
+  //"macron",
+  "zemmour",
+  "melenchon",
+  "philippot",
+  "poutou",
+  "roussel",
+  "mlp",
+  "pecresse",
+  "jadot",
+  "asselineau",
+  "sandrousseau",
+  //"hidalgo",
+  "kazib",
+  "taubira",
+  //"lassalle",
+  "dupontaignan",
+  "bertrand",
+  "arthaud",
+  "barnier",
+  "montebourg",
+];
 const nodeColors = [
   0x416D19,
   0xB7C9F2,
   0xB4B4B8,
   0xE8C872,
   0xD04848
-]
+];
 
 function setup() {
   scene = new THREE.Scene();
@@ -227,26 +254,12 @@ function buildCrossingEdges(edgesData, nodesData, key) {
   nodesData = nodesData.flat();
 	// orientamos y posicionamos cada instancia
   edgesData.forEach((edgeData, idx) => {
-    const node1 = nodesData.find((node) => node[0] == edgeData[3]);
+    const node1 = nodesData.find((node) => node[0] == edgeData[0]);
     const node2 = nodesData.find((node) => node[0] == edgeData[4]);
     
     if (node1 && node2) {
-      const sourceNode = getNodePositionById(instancedNodes, node1[0]);
-      const targetNode = getNodePositionById(instancedNodes, node2[0]);
-      const sourceGroupId = edgeData[2];
-      const targetGroupId = edgeData[1];
-
-      for (let i = 0; i < Object.keys(partiesJson).length; i++) {
-        if (partiesJson[Object.keys(partiesJson)[i]].id === sourceGroupId) {
-          var sourceGroup = Object.keys(partiesJson)[i];
-        }
-        if (partiesJson[Object.keys(partiesJson)[i]].id === targetGroupId) {
-          var targetGroup = Object.keys(partiesJson)[i];
-        }
-      }
-
-      let source = new THREE.Vector3(sourceNode[0] + partiesJson[sourceGroup].position.x, sourceNode[1] + partiesJson[sourceGroup].position.y, sourceNode[2] + partiesJson[sourceGroup].position.z);
-		  let target = new THREE.Vector3(targetNode[0] + partiesJson[targetGroup].position.x, targetNode[1] + partiesJson[targetGroup].position.y, targetNode[2] + partiesJson[targetGroup].position.z);
+      let source = new THREE.Vector3(edgeData[1], edgeData[2], edgeData[3]);
+		  let target = new THREE.Vector3(edgeData[5], edgeData[6], edgeData[7]);
 
       translationMatrix.makeTranslation(source.x, source.y, source.z);
       rotMatrix.lookAt(source, target, new THREE.Vector3(0,1,0))
@@ -266,150 +279,65 @@ function buildCrossingEdges(edgesData, nodesData, key) {
 	return instancedCrossingEdges;
 }
 
-// BORRAR
-function drawLine(x, y, color) {
-  const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
-  const points = [];
-  points.push(
-    new THREE.Vector3(
-      parseFloat(x),
-      parseFloat(y),
-      0
-    )
-  );
-  points.push(
-    new THREE.Vector3(
-      x,
-      y,
-      0
-    )
-  );
-  const geometry = new THREE.BufferGeometry().setFromPoints(points);
-  const line = new THREE.Line(geometry, material);
-  scene.add(line);
-}
-
 function calculateGraphPositions() {
-  //const totalSize = Object.values(partiesJson).reduce((acc, {lines}) => acc + lines, 0);
-  //console.log("total size", totalSize);
   const keys = Object.keys(partiesJson);
   const radius = 400;
-  //let currentAngle = 0
   const angleIncrement = Math.PI / 18
 
   for (let i = 0; i < keys.length; i++) {
     const key = keys[i];
-    //const graphSize = partiesJson[key].lines;
-    //const angleSegment = (graphSize / totalSize) * Math.PI;
-    //const angle = currentAngle + (graphSize / totalSize) * Math.PI;
     const angle = i * angleIncrement;
     const x = radius * Math.cos(angle); // convert polar coordinates to cartesian coordinates
     const y = radius * Math.sin(angle) - 150;
     const z = 0; // can be adjusted to position along the z-axis
     partiesJson[key].position = { x, y, z };
-    // currentAngle += angleSegment
   }
   return partiesJson;
-}
-
-function positionNodesInSemiCircle(graphSize, index) {
-  const totalGraphSize = 35338;
-  const sizePercentage = graphSize / totalGraphSize * 100;
-  const numSubGraphs = 20;
-  const radius = 350;
-  const positionsForSubGraphs = [];
-  const angleIncrement = (Math.PI / (totalGraphSize + 1));
-
-  let currentAngle = 0
-
-  for (let i = 1; i <= numSubGraphs; i++) {
-      // const angle = i * angleIncrement;
-      const angle = currentAngle + (graphSize / totalGraphSize) * Math.PI;
-      const x = radius * Math.cos(angle); // convert polar coordinates to cartesian coordinates
-      const y = radius * Math.sin(angle) - 150;
-      const z = 0; // can be adjusted to position along the z-axis
-
-      positionsForSubGraphs.push({ x, y, z });
-      currentAngle = angle
-  }
-
-  return positionsForSubGraphs;
-}
-
-/**
-  function positionNodesInSemiCircle(subGraphSizes) {
-    const numSubGraphs = subGraphSizes.length;
-    const radius = 500;
-    const positionsForSubGraphs = [];
-    const totalSize = subGraphSizes.reduce((acc, size) => acc + size, 0);
-    const angleIncrement = Math.PI / (totalSize + 1); // Distribuye los subgrafos proporcionalmente al tamaño total
-
-    let currentAngle = 0;
-
-    for (let i = 0; i < numSubGraphs; i++) {
-      const size = subGraphSizes[i];
-      const angle = currentAngle + (size / totalSize) * Math.PI;
-      const x = radius * Math.cos(angle); // convertir coordenadas polares a coordenadas cartesianas
-      const y = radius * Math.sin(angle);
-      const z = 0; // puede ajustarse para posicionar a lo largo del eje z
-
-      positionsForSubGraphs.push({ x, y, z });
-      currentAngle = angle;
-    }
-
-    return positionsForSubGraphs;
-  }
-
- */
-
-/*function positionNodesInSemiCircle(graphs) {
-  // Calcular el tamaño total de todos los subgrafos
-  let totalSize = 35338;
-
-  // Calcular el ángulo total del semicírculo (en radianes)
-  let totalAngle = Math.PI;
-
-  // Inicializar el ángulo actual en el que estamos posicionando nodos
-  let currentAngle = 0;
-
-  for (let graph of graphs) {
-      // Calcular el ángulo que este subgrafo debe ocupar en el semicírculo
-      let graphAngle = (graph.size / totalSize) * totalAngle;
-
-      // Posicionar los nodos en este subgrafo
-      for (let node of graph.nodes) {
-          // Calcular la posición del nodo en el semicírculo
-          let x = Math.cos(currentAngle);
-          let y = Math.sin(currentAngle);
-
-          // Actualizar la posición del nodo
-          node.position = {x, y};
-
-          // Avanzar el ángulo actual para el próximo nodo
-          currentAngle += graphAngle / graph.nodes.length;
-      }
-  }
-}*/
-
-function positionNodesInLine() {
-  const spaceBetweenSubGraphs = 60;
-  const numSubGraphs = 21;
-  const startPositionX = -((numSubGraphs - 1) * spaceBetweenSubGraphs) / 2;
-  const positionsForSubGraphs = [];
-  for (let i = 0; i < numSubGraphs; i++) {
-      const positionX = startPositionX + i * spaceBetweenSubGraphs;
-      const positionY = 0; // Can be adjusted to position along the y-axis
-      const positionZ = 0; // Can be adjusted to position along the z-axis
-      positionsForSubGraphs.push({ x: positionX, y: positionY, z: positionZ });
-  }
-
-  return positionsForSubGraphs;
 }
 
 function drawCrossingEdges(edges, nodes, key="poutou") {
   console.log("crossing edges", edges);
   instancedEdges = buildCrossingEdges(edges, nodes, key);
   scene.add(instancedEdges);
+}
+
+function preProcessCrossingEdges(crossingEdgesData) {
+  crossingEdgesData.forEach((crossingEdge) => {
+    //console.log("crossing edge", crossingEdge);
+    const sourceId = crossingEdge[3];
+    const targetId = crossingEdge[4];
+    const source = getNodePositionById(instancedNodes, sourceId);
+    const target = getNodePositionById(instancedNodes, targetId);
+    const sourceGroupId = crossingEdge[2];
+    const targetGroupId = crossingEdge[1];
+    //console.log("source", source);
+    //console.log("sourceId", sourceId);
+    //console.log("sourceGroupId", sourceGroupId);
+    //console.log("target", target);
+    //console.log("targetId", targetId);
+    //console.log("targetGroupId", targetGroupId);
+    for (let i = 0; i < Object.keys(partiesJson).length; i++) {
+      if (partiesJson[Object.keys(partiesJson)[i]].id === sourceGroupId) {
+        var sourceGroup = Object.keys(partiesJson)[i];
+      }
+      if (partiesJson[Object.keys(partiesJson)[i]].id === targetGroupId) {
+        var targetGroup = Object.keys(partiesJson)[i];
+      }
+    }
+    //console.log("sourceGroup", sourceGroup);
+    //console.log("targetGroup", targetGroup);
+    const sourceX = source[0] + partiesJson[sourceGroup].position.x
+    const sourceY = source[1] + partiesJson[sourceGroup].position.y
+    const sourceZ = source[2] + partiesJson[sourceGroup].position.z
+    const targetX = target[0] + partiesJson[targetGroup].position.x
+    const targetY = target[1] + partiesJson[targetGroup].position.y
+    const targetZ = target[2] + partiesJson[targetGroup].position.z
+    const edge = [sourceId, sourceX, sourceY, sourceZ, targetId, targetX, targetY, targetZ];
+    //console.log("edge", edge);
+    crossing_edges.push(edge);
+  });
+  return crossing_edges;
+
 }
 
 function drawGraph(nodes, edges, key, partiesData) {
@@ -433,34 +361,6 @@ const animate = function () {
 
 async function loadData() {
   const partiesData = calculateGraphPositions();
-  const nodeFilePrefix = "/data/nodes/mcgs_reduced_";
-  const edgeFilePrefix = "/data/edges/mcgs_reduced_";
-  const edgeFileSuffix = ".csv";
-  const nodeFileSuffix = "_FR.csv";
-  const fileKeys = [
-    //"macron",
-    "zemmour",
-    "melenchon",
-    "philippot",
-    "poutou",
-    "roussel",
-    "mlp",
-    "pecresse",
-    "jadot",
-    "asselineau",
-    "sandrousseau",
-    //"hidalgo",
-    "kazib",
-    "taubira",
-    //"lassalle",
-    "dupontaignan",
-    "bertrand",
-    "arthaud",
-    "barnier",
-    "montebourg",
-  ];
-
-  // secuencial
 
   for (let i = 0; i < fileKeys.length; i++) {
     const nodeFile = nodeFilePrefix + fileKeys[i] + nodeFileSuffix;
@@ -472,34 +372,16 @@ async function loadData() {
     animate();
   }
   return nodes_crossing;
-  
+}
 
-  // asincrono
-  /*fileKeys.forEach(async (key, idx) => {
-    const nodeFile = nodeFilePrefix + key + nodeFileSuffix;
-    const edgeFile = edgeFilePrefix + key + edgeFileSuffix;
-    loadCSV(nodeFile)
-      .then((nodesData) => {
-        loadCSV(edgeFile)
-          .then((edgesData) => {
-            drawGraph(nodesData, edgesData, idx);
-            animate();
-          })
-          .catch(console.error)
-      })
-      .catch(console.error)
-  })*/
+async function loadCrossingEdges(nodes_crossing) {
+  const crossingEdgesData = await loadCSV(crossingEdgeFile);
+  crossing_edges = preProcessCrossingEdges(crossingEdgesData);
+  drawCrossingEdges(crossing_edges, nodes_crossing);
+  animate();
 }
 
 let nodes_crossing = [];
 setup();
 nodes_crossing = await loadData();
-const crossingEdges = await loadCSV("/data/edges/df_crossed_edges.csv");
-drawCrossingEdges(crossingEdges, nodes_crossing);
-animate();
-/*.then(async (crossing) => {
-  console.log("nodes_crossing", crossing.length)
-  const crossingEdges = await loadCSV("/data/edges/df_crossed_edges.csv");
-  drawCrossingEdges(crossingEdges, crossing);
-  animate();
-});*/
+await loadCrossingEdges(nodes_crossing);
